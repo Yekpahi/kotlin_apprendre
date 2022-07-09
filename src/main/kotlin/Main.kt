8bc1abf91a6ffa0c8ef1be0fcfde2e0e59003c41
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-07-10 01:08:11 +0200

    When if etc.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import java.util.Scanner
 fun main(args: Array<String>) {
     /*val tens1 : String = "Je m'appelle "
     val nom : String = "Marc, j'ai  "
@@ -23,11 +24,82 @@ fun main(args: Array<String>) {
     }
     print(z)
      */
-
+    /*
     val num = intArrayOf(10, 30, 54, -40)
     if(10 in num) {
         println(num[0])
     }
     num[1] = 45
     print(num[1] )
+     */
+
+    /*
+    val score : Int
+    score = 90 + 25
+    println(" score = $score")
+     */
+
+   /* val a = 19
+    val b = 13
+    val max : Int
+    max = if (a>b) a - b  else b - a
+    print("$max")
+    */
+// input
+   /* print("Entrer votre texte: ")
+
+    val stringInput = readLine()!!
+    print("Ton texte est : $stringInput")
+    */
+
+    /*
+    val reader = Scanner(System.`in`)
+    print("Entrer un chiffre :  ")
+
+    var integer : Int = reader.nextInt()
+
+    println("You entered * 10: ${integer*10}")
+     */
+// WHEN STATEMENT
+
+    /*val a = 12
+    val b = 5
+
+    println("Enter Operator either +, -, * or / : ")
+    val operator = readLine()
+
+    val result = when (operator) {
+        "+" -> a + b
+        "-" -> a - b
+        "*" -> a * b
+        "/" -> a / b
+        else -> "$operator operator is invalid operator."
+    }
+
+    println("result = $result")
+     */
+    /*val a = 12
+    val b = 5
+
+    println("Enter operator either +, -, * or / : ")
+    val operator = readLine()
+
+    when (operator) {
+        "+" -> println("$a + $b = ${a + b}")
+        "-" -> println("$a - $b = ${a - b}")
+        "*" -> println("$a * $b = ${a * b}")
+        "/" -> println("$a / $b = ${a / b}")
+        else -> println("$operator is invalid")
+    }
+*/
+    val reader = Scanner(System.`in`)
+    print("Entrer un chiffre :  ")
+
+    var chiffre : Int = reader.nextInt()
+
+    when (chiffre) {
+        10, 12, 30-> println("$chiffre is a positive integer less than 4.")
+        62,54, 100 -> println("$chiffre is a positive integer less than 1000.")
+        else -> println("$chiffre is invalid")
+    }
 }
