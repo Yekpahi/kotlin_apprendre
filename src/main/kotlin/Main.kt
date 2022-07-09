c56ceec63f6fd0193dcd13d9b193a7bf0245f59a
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-07-09 14:39:46 +0200

    operators

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,33 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    /*val tens1 : String = "Je m'appelle "
+    val nom : String = "Marc, j'ai  "
+    val age : Int = 13
+    val tens2 : String = " ans."
+    val result = tens1 + nom + age + tens2
+    println(result)
+     */
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    /*var num = 12
+    num *=10
+    println("num * 10 = $num")
+     */
+
+    /*val x = 12
+    val y = 65
+    val z = if (x>y) {
+        val result = x - y
+        println("x - y = $result")
+    } else {
+        val result = y - x
+        println("y - x = $result")
+    }
+    print(z)
+     */
+
+    val num = intArrayOf(10, 30, 54, -40)
+    if(10 in num) {
+        println(num[0])
+    }
+    num[1] = 45
+    print(num[1] )
 }
